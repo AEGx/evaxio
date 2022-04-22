26206bf53f5de850b86655fdbd01b14ebeb1c7ca
maki <maknaitammar@gmail.com>
2022-04-22 15:55:32 +0200

    update small change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    publicRuntimeConfig:{
+        //Public var env
+    },
+    privateRuntimeConfig: {
+        //Private var env
+    },
     typescript: {
         strict: true
       },
